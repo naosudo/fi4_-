aaeb52ac382507ad68100c6f0fc79595e6ed6eea
naosudo <nao@nicoriwa.com>
2025-08-15 07:07:59 +0900

    First commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,58 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  def index
+    @items = Item.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    redirect_to root_path unless current_user == @item.user
+  end
+  def update
+    # 自身の出品商品以外は編集不可
+    unless current_user == @item.user
+      redirect_to root_path and return
+    end
+
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "商品情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :image,
+      :price,
+      :description,
+      :category_id,
+      :status_id,
+      :payment_id,
+      :prefecture_id,
+      :day_id
+    ).merge(user_id: current_user.id)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  
+  root to: "items#index"
+  
+end
